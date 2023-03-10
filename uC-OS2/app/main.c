ed209789499ea503ff3f2c29d1d90ac22ba7fcad
guystone233 <2420081099@qq.com>
2023-03-10 14:57:50 +0800

    Update main.c

--- uC-OS2/app/main.c
+++ uC-OS2/app/main.c
@@ -1,16 +1,16 @@
 #include "led.h"
 #include "stm32f4xx.h"
 #include "ucos_ii.h"
-#define STARTUP_TASK_PRIO 1
-#define STARTUP_TASK_STK_SIZE 128
-static OS_STK startup_task_stk[STARTUP_TASK_STK_SIZE]; // 定义栈
+#define TASK_PRIO 1
+#define TASK_STK_SIZE 128
+static OS_STK task_stk[TASK_STK_SIZE]; // 定义栈
 
 int main(void)
 {
 	InitLED();
 	OSInit();
 	OSTaskCreate(FlashLED, (void *)0,
-				 &startup_task_stk[STARTUP_TASK_STK_SIZE - 1], STARTUP_TASK_PRIO);
+				 &task_stk[TASK_STK_SIZE - 1], TASK_PRIO);
 	OSStart();
 	return 0;
 }
